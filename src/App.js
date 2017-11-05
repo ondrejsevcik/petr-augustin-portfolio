2e3ffd55e42392deb1e03de964cc8cd604c30fd0
Ondrej Sevcik <ondrej.sev@gmail.com>
2017-11-05 11:26:01 +0100

    Add article

--- src/App.js
+++ src/App.js
@@ -29,17 +29,30 @@ class App extends Component {
         <section 
           className="row hero-banner"
         >
-          <img 
-            className="hero-banner__img"
-            src="http://petraugustin.com/wp-content/uploads/2017/02/cropped-st3-1.png" 
-          />
-          <div className="hero-banner__titles">
-            <h1 className="hero-banner__title">
-              My name is Petr Augustyn<span className="hero-banner__dot">.</span>
-            </h1>
-            <h2>I design ...</h2>
+          <div className="col-12 hero-banner__wrapper">
+            <img 
+              className="hero-banner__img"
+              src="http://petraugustin.com/wp-content/uploads/2017/02/cropped-st3-1.png" 
+            />
+            <div className="hero-banner__titles">
+              <h1 className="hero-banner__title">
+                My name is Petr Augustyn<span className="hero-banner__dot">.</span>
+              </h1>
+              <h2>I design ...</h2>
+            </div>
           </div>
         </section>
+        <main className="row">
+          <article className="col-6">
+            <a href="/" className="article__link">
+              <img
+                className="article__image"
+                src="http://petraugustin.com/wp-content/uploads/2017/10/final5.png"
+              />
+              <h1>Weather &amp; fuel aircraft app</h1>
+            </a>
+          </article>
+        </main>
       </div>
     );
   }
